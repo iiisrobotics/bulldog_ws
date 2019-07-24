02dbf41b786c08dbddde8324ddf28b38c1b8c9f9
Chongyi Zheng <chongyeezheng@gmail.com>
2019-07-24 14:27:20 +0800

    add bulldog_grasp_demo world

--- src/gpd/scripts/bulldog_gpd.py
+++ src/gpd/scripts/bulldog_gpd.py
@@ -145,7 +145,7 @@ def process_cloud(cloud_transformed, detection):
 	if len(detection.masks) > 0:
 		for class_name, mask in zip(detection.class_names,
 									detection.masks):
-			if class_name == 'bottle' or 'cup':
+			if (class_name == 'bottle') or (class_name == 'cup'):
 				mask_data = np.fromstring(mask.data, dtype=np.uint8)
 				one_index = np.nonzero(mask_data)[0]
 				cloud_indexed.indices = [Int64(idx) for idx in one_index]
@@ -270,7 +270,7 @@ def main():
 	# group.execute(plan, wait=True)
 
 	rospy.loginfo("============ Waiting while RVIZ displays motion planning...")
-	rospy.sleep(3.0)
+	rospy.sleep(10.0)
 
 
 if __name__ == "__main__":
