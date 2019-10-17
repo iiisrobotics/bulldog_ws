4fbc0d624206b63806566fe14a4b1ecf00fc0b3b
Chongyi Zheng <chongyeezheng@gmail.com>
2019-10-17 19:24:16 +0800

    add pick_and_place

---
+++ src/pick_and_place/src/node/pick_and_place_service_node.cpp
@@ -0,0 +1,16 @@
+//---------- pick_and_place
+#include "pick_and_place/lib/pick_and_place_server.h"
+//----------
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "pick_and_place_service_node");
+
+    ros::NodeHandle node("~");
+    
+    pick_and_place::PickAndPlaceServer pick_and_place_server(node);
+    pick_and_place_server.launch();
+
+    ros::waitForShutdown();
+    return 0;
+}
